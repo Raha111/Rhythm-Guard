c5483dcc0704b8a7735c1118875d9437ff6f9a2d
tasfiatasnimraisa <tasfiatasnim387@gmail.com>
2023-06-28 08:28:13 +0600

    regintration-login added

---
+++ app/src/main/java/com/example/rhythym_guard/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.example.rhythym_guard;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText phone,password;
+    Button btn;
+    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child("users");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        phone=(EditText) findViewById(R.id.inputPhone2);
+        password=(EditText) findViewById(R.id.inputPassword2);
+        btn=(Button) findViewById(R.id.btn2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String sphone=phone.getText().toString();
+                String spassword=password.getText().toString();
+                if(sphone.isEmpty()||spassword.isEmpty())
+                {
+                    Toast.makeText(LoginActivity.this, "PLEASE ENTER YOUR MOBILE OR PASSWORD", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(sphone))
+                            {
+                                String getpass=snapshot.child(sphone).child("Password").getValue(String.class);
+                                if(getpass.equals(spassword))
+                                {
+                                    Intent intent;
+                                   intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                                   // intent.putExtra("phone",sphone);
+                                    startActivity(intent);
+
+                                }
+                                else
+                                {
+                                    Toast.makeText(LoginActivity.this,"WRONG PASSWORD",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/rhythym_guard/RegisterActivity.java
@@ -0,0 +1,51 @@
+package com.example.rhythym_guard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText username,email,phone,password,cpassword;
+    private Button btn;
+    private TextView textview;
+    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child("users");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        username=findViewById(R.id.inputUsername);
+        email=findViewById(R.id.inputEmail);
+        phone=findViewById(R.id.inputPhone2);
+        password=findViewById(R.id.inputPassword2);
+        cpassword=findViewById(R.id.inputConfirmPassword);
+        btn=findViewById(R.id.btnRegister);
+        textview=findViewById(R.id.textView2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String st_phone=phone.getText().toString();
+                databaseReference.child("users").child(st_phone).child("Phone").setValue(st_phone);
+
+                String st_username=username.getText().toString();
+                databaseReference.child("users").child(st_phone).child("UserName").setValue(st_username);
+                //String st_email=email.getText().toString();
+                //databaseReference.child("users").child(st_phone).child("Email").setValue(st_email);
+                String st_password=password.getText().toString();
+                databaseReference.child("users").child(st_phone).child("Password").setValue(st_password);
+               Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
